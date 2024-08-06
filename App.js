1a4751505da8bebc128cec5860e0cd495b4882cb
Doğuhan İlter <doguhannilt@gmail.com>
2024-08-06 03:05:14 +0300

    @reduxjs/toolkit installed

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 // REDUX
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 // SCREENS
 import Home from './screens/Home';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
+    </Provider>
   );
 }
 
