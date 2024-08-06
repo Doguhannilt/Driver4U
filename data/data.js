37b99ed4a926a53269befa93dc85628cefb017ce
Doğuhan İlter <doguhannilt@gmail.com>
2024-08-06 04:40:56 +0300

    Fake data added

---
+++ data/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        id: "1823891",
+        title: "Rider",
+        image: "https://images.pexels.com/photos/977213/pexels-photo-977213.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        screen: "MapScreen",
+        description: "Find and book a ride quickly with our easy-to-use ride-hailing service.",
+        rating: 4.8,
+        estimatedTime: "Avg 5-10 min",
+        category: "Transport"
+    },
+    {
+        id: "18238943242342342431",
+        title: "Order Food",
+        image: "https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        screen: "EatsScreen",
+        description: "Order your favorite food from top-rated restaurants near you.",
+        rating: 4.7,
+        estimatedTime: "Avg 20-30 min",
+        category: "Food Delivery"
+    },
+    {
+        id: "543543435435",
+        title: "Alcohol Food",
+        image: "https://zinniahealth.com/_next/image?url=https%3A%2F%2Fcdn.zinniahealth.com%2Fwp-content%2Fuploads%2F20221026132959%2FiStock-475273684-scaled.jpg&w=640&q=75",
+        screen: "AlcoholScreen",
+        description: "Browse and order a variety of alcoholic beverages from our extensive selection.",
+        rating: 4.5,
+        estimatedTime: "Avg 15-25 min",
+        category: "Alcohol Delivery"
+    },
+    {
+        id: "39284892034980",
+        title: "Entertainment",
+        image: "https://media.licdn.com/dms/image/C4E12AQH0nqdPDxOz7g/article-cover_image-shrink_720_1280/0/1635189014664?e=1728518400&v=beta&t=ILulBzus9sm_LJjz13oDl86Nz0PlZsrWhQQh8uH6JGE",
+        screen: "MoviesScreen",
+        description: "Book tickets for the latest movies at nearby theaters.",
+        rating: 4.7,
+        estimatedTime: "Varies",
+        category: "Entertainment"
+    }
+];
