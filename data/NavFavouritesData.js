04154fec725fce03594414c757fd51e03993df6a
Doğuhan İlter <doguhannilt@gmail.com>
2024-08-07 20:37:47 +0300

    Fake data created

---
+++ data/NavFavouritesData.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        id: "12321312",
+        icon: "home",
+        location: "home",
+        destination: "Code Street, London Uk"
+    },
+    {
+        id: "1231232123",
+        icon: "briefcase",
+        location: "work",
+        destination:"London Eye, London, UK"
+    }
+]
---
+++ data/driverforyoucars.js
@@ -0,0 +1,20 @@
+export const data = [
+    {
+        id: "Uber-X-123",
+        title: "UberX",
+        multiplier: 1,
+        image:"https://links.papareact.com/3pn"
+    },
+    {
+        id: "Uber-XL-456",
+        title: "Uber XL",
+        multiplier: 1.2,
+        image:"https://links.papareact.com/3pn"
+    },
+    {
+        id: "Uber-LUX-789",
+        title: "Uber LUX",
+        multiplier: 1.75,
+        image:"https://links.papareact.com/3pn"
+    }
+]
