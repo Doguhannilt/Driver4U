38fa0be312729e285015283dfd5c8b0613901457
Doğuhan İlter <doguhannilt@gmail.com>
2024-08-09 20:30:27 +0300

    index.js is created | dotenv | cookieParser | connectiontoDB

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+// Packages
+
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
+// Utils
+import connectiontoDB from './config/database.js'
+
+// Routes
+import userRoutes from './routes/userRoutes.js'
+
+dotenv.config()
+const port = process.env.PORT || 5000
+
+connectiontoDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+// Route
+app.use('/api/users', userRoutes)
+
+
+
+app.listen(port, () => console.log(`Server running on port: ${port}`)) 
